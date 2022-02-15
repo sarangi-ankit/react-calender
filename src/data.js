fd72b33cb44024a16eee177c58cc82cede8d2102
Ankit Sarangi <sarangiankit097@gmail.com>
2022-02-15 22:36:51 +0530

    done

---
+++ src/data.js
@@ -0,0 +1,52 @@
+const Data= [{
+  "Id": 1,
+  "Subject": "Explosion of Betelgeuse Star",
+  "Location": "Space Centre USA",
+  "StartTime": "2019-01-06T04:00:00.000Z",
+  "EndTime": "2019-01-06T05:30:00.000Z",
+  "CategoryColor": "#1aaa55"
+}, {
+  "Id": 2,
+  "Subject": "Thule Air Crash Report",
+  "Location": "Newyork City",
+  "StartTime": "2019-01-07T06:30:00.000Z",
+  "EndTime": "2019-01-07T08:30:00.000Z",
+  "CategoryColor": "#357cd2"
+},{
+  "Id": 111,
+  "Subject": "Explosion of Betelgeuse Star",
+  "Location": "Space Centre USA",
+  "StartTime": "2019-01-06T04:00:00.000Z",
+  "EndTime": "2019-01-06T05:30:00.000Z",
+  "CategoryColor": "#1aaa55"
+}, {
+  "Id": 112,
+  "Subject": "Thule Air Crash Report",
+  "Location": "Newyork City",
+  "StartTime": "2019-01-07T06:30:00.000Z",
+  "EndTime": "2019-01-07T08:30:00.000Z",
+  "CategoryColor": "#357cd2"
+},{
+  "Id": 113,
+  "Subject": "Explosion of Betelgeuse Star",
+  "Location": "Space Centre USA",
+  "StartTime": "2019-01-06T04:00:00.000Z",
+  "EndTime": "2019-01-06T05:30:00.000Z",
+  "CategoryColor": "#1aaa55"
+}, {
+  "Id": 114,
+  "Subject": "Thule Air Crash Report",
+  "Location": "Newyork City",
+  "StartTime": "2019-01-07T06:30:00.000Z",
+  "EndTime": "2019-01-07T08:30:00.000Z",
+  "CategoryColor": "#357cd2"
+},{
+  "Id": 115,
+  "Subject": "Explosion of Betelgeuse Star",
+  "Location": "Space Centre USA",
+  "StartTime": "2019-01-06T04:00:00.000Z",
+  "EndTime": "2019-01-06T05:30:00.000Z",
+  "CategoryColor": "#1aaa55"
+}]
+
+export default Data
